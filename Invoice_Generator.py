0189fad1febfd88d98c1c8adcbebaa0b383fb4b7
bitmap357 <edakoyeram@gmail.com>
2023-07-21 23:32:20 +0000

    font

--- Invoice_Generator.py
+++ Invoice_Generator.py
@@ -42,7 +42,7 @@ def generate_invoice():
 
     pdf = FPDF()
     pdf.add_page()
-
+    pdf.set_font("Helvetica", size=12)
     pdf.cell(0, 10, txt="Invoice", new_x="LMARGIN", new_y="NEXT", align="C")
     pdf.cell(0, 10, txt="Customer: "+customer_name, new_x="LMARGIN", new_y="NEXT", align="L")
 
