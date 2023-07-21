7b4d6e322e665c6d0e6cac9a0633ba7f466a5e60
bitmap357 <edakoyeram@gmail.com>
2023-07-21 21:46:43 +0000

    medicine listbox

--- Invoice_Generator.py
+++ Invoice_Generator.py
@@ -6,4 +6,7 @@ window.title("Invoice Generator")
 medicine_label = Label(window, text="Medicine: ")
 medicine_label.pack()
 
+medicine_listbox = Listbox(window, selectmode=SINGLE)
+medicine_listbox.pack()
+
 window.mainloop()
